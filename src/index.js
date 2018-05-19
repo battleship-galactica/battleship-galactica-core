6f0357f9bb1db96a34b3ae0475736b006c44bd74
Sagar Patke <sagar.patke@stackroute.in>
2018-05-20 01:27:04 +0530

    Configure istanbul for code coverage (integration branch)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = (bool) => {
+  switch(bool) {
+    case true:
+      return true;
+    case false:
+      return false;
+  }
+}
--- src/test.spec.js
+++ src/test.spec.js
@@ -1,7 +1,17 @@
+const f = require('.');
+
 describe('mocha tests for `src/**/*.(spec|test).js`', function () {
   it('should pass', function (done) {
     setTimeout(() => {
       done();
     }, 100);
+  });
+});
+
+describe('istanbul', function() {
+  it('1/2 true branch', function(done) {
+    f(true);
+    f(false);
+    done();
   });
 });
