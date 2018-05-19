3533a8014748c0397331d7efe35d65eb2e97542f
Sagar Patke <sagar.patke@stackroute.in>
2018-05-20 00:17:36 +0530

    Setup NodeJS project with redux, rxjs, mocha and chai

---
+++ src/test.spec.js
@@ -0,0 +1,7 @@
+describe('mocha tests for `src/*.spec.js`', function() {
+	it('should pass', function(done) {
+		setTimeout(() => {
+			done();
+		}, 100);
+	});
+});
---
+++ test/test.js
@@ -0,0 +1,7 @@
+describe('mocha test from `test` directory', function() {
+	it('should pass', function(done) {
+		setTimeout(() => {
+			done();
+		}, 100);
+	});
+});
